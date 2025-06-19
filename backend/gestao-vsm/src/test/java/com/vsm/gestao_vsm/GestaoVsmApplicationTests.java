eb9bc2222a19ce86a85b89420ca6dd3501cf15b2
Caio Sabino <caiomsabino@gmail.com>
2025-06-19 11:58:04 -0300

    add: pasta backend e proj spring

---
+++ backend/gestao-vsm/src/test/java/com/vsm/gestao_vsm/GestaoVsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsm.gestao_vsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
