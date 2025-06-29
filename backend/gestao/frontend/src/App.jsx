1c2c6d9204567b1d67ef6501d012be36243e84af
Weverton Rodrigues <mr.weverton1226@gmail.com>
2025-06-29 20:25:02 -0300

    feat: adição de tela de agendamento mockado

--- backend/gestao/frontend/src/App.jsx
+++ backend/gestao/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AgendaPage from './pages/AgendaPage.jsx';
+import AgendaMock from './pages/AgendaMock.jsx';
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme.js'; 
 
@@ -9,7 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<App />} /> */}
-          <Route path="/" element={<AgendaPage />} /> {/* trocar path depois */}
+          {/*<Route path="/" element={<AgendaPage />} />  trocar path depois */}
+          <Route path="/" element={<AgendaMock />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
